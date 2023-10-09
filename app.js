bb8308740cf8602b4f3478ac974af0aaf4bb9d0e
Deepak <124507265+dpk-smartcoder@users.noreply.github.com>
2023-10-09 18:39:07 +0530

    Header and Footer added

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app=express();
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.get("/",(req,res)=>{
-    res.render("home");
+    res.render("layouts/header");
 });
 app.listen(3000,()=>{
     console.log("server started");
